44d13fe5edc63047487d12a9808169332ce71305
fahad-sarwar <mohammed_fahad00@hotmail.com>
2018-07-10 23:18:48 +0100

    created shopping cart spec

--- Tests/test_shoppingCartSpec.py
+++ Tests/test_shoppingCartSpec.py
@@ -1,5 +1,28 @@
-def test_answer():
-    assert inc(3) == 4
+import pytest
 
-def inc(x):
-    return x + 1
+@pytest.mark.parametrize("items, cost", [
+    (["A"], 50),
+    (["B"], 30),
+    (["A", "B", "C", "D"], 105),
+    (["A", "A", "A"], 130)
+])
+def test_checkout_total(items, cost):
+    checkout = Checkout()
+    for item in items: checkout.Scan(item)
+    assert checkout.Total() == cost
+
+class Checkout():
+    def __init__(self):
+        self._prices = {"A":50, "B":30, "C":15, "D":10}
+        self._total = 0
+        self._basket = []
+
+    def Scan(self, sku):
+        self._total += self._prices[sku]
+        self._basket.append(sku)
+
+    def Total(self):
+        if(self._basket.count("A") == 3):
+            self._total -= 20
+
+        return self._total
