276c16d9e6d61bf74212541dbbd4b8a22fc88a23
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2018-07-16 18:44:36 +0100

    Updated spec test

--- Tests/test_shoppingCartSpec.py
+++ Tests/test_shoppingCartSpec.py
@@ -1,19 +1,25 @@
 import pytest
 
+
 @pytest.mark.parametrize("items, cost", [
     (["A"], 50),
     (["B"], 30),
     (["A", "B", "C", "D"], 105),
-    (["A", "A", "A"], 130)
+    (["A", "A", "A"], 130),
+    (["B", "B"], 45)
 ])
 def test_checkout_total(items, cost):
     checkout = Checkout()
-    for item in items: checkout.Scan(item)
+
+    for item in items:
+        checkout.Scan(item)
+
     assert checkout.Total() == cost
+
 
 class Checkout():
     def __init__(self):
-        self._prices = {"A":50, "B":30, "C":15, "D":10}
+        self._prices = {"A": 50, "B": 30, "C": 15, "D": 10}
         self._total = 0
         self._basket = []
 
@@ -25,4 +31,33 @@ class Checkout():
         if(self._basket.count("A") == 3):
             self._total -= 20
 
+        if(self._basket.count("B") == 2):
+            self._total -= 15
+
         return self._total
+
+
+class Discounter():
+    def __init__(self):
+        self._rules = [DiscountRule("A", 3, 20), DiscountRule("B", 2, 15)]
+
+    def GetDiscountTotal(self, basket):
+        totalDiscount = 0
+
+        for rule in self._rules:
+            checkout.Scan(item)
+
+
+class DiscountRule():
+    def __init__(self, sku, numberOfItems, discountAmount):
+        self._sku = sku
+        self._numberOfItems = numberOfItems
+        self._discountAmount = discountAmount
+
+    def GetDiscount(self, basket):
+        countOfSkus = basket.count(self._sku)
+
+        if(countOfSkus == self._numberOfItems):
+            return self._discountAmount
+
+        return 0
